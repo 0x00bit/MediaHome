d64a3e096d9460602c765bdb1b21de304a42e87d
0x00bit <136848260+0x00bit@users.noreply.github.com>
2024-05-15 11:24:12 -0300

    Add files via upload
    feat/fix: added logout endpoint and fixed multi-user conflicts

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/bin/bash python3
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, jsonify
 from smbmodule import SmbConnection
 from smbprotocol import exceptions as ex
 
@@ -15,39 +15,54 @@ class Server:
         self.app.secret_key = "cookedpotatos"
 
     def create_server(self):
+
         '''
         This function is responsable for creating and
         setup whole server
         '''
+
         @self.app.route('/', methods=['GET', 'POST'])
         def home():
             return render_template('index.html'), 200
 
+        # Endpoint to logout
+        @self.app.route('/logout', methods=['GET'])
+        def logout():
+            print(session, dir(session))
+            session.pop("session", None)
+            return redirect("/", code=200)
+
+        # Authentication page
         @self.app.route('/login', methods=['GET', 'POST'])
         def login_page():
             if request.method == 'POST':
-                self.username = request.form['username']
-                self.password = request.form['password']
-                self.smb_server._user = self.username
-                self.smb_server._passwd = self.password
-                try:
-                    smb_session = self.smb_server.start_conn()
-                    if smb_session._connected is True:
-                        session["username"] = self.username
-                        return redirect("/user", code=302)
-                    else:
-                        print(smb_session._connected)
-                        print(smb_session)
+                self.smb_server._user = request.form['username']
+                self.smb_server._passwd = request.form['password']
 
-                except ex.SMBException:
-                    return render_template('bad_login.html'), 401
+                # Check is the session already exist
+                if self.smb_server._user in session:
+                    session.pop(self.smb_server._user, None)
+                    print("Removing existing session...")
+                
+                # If doesn't exist, then, try instance a new connection with server
+                else:
+                    try:
+                        smb_session = self.smb_server.start_conn()
+                        if smb_session._connected is True:
+                            session[self.smb_server._user] = smb_session.session_key
+                            return redirect("/user", code=302)
+
+                    except ex.SMBException:
+                        return render_template('bad_login.html'), 401
 
             return render_template('login.html')
 
+        # Default page of user directory
         @self.app.route('/user', methods=['GET', 'POST'])
         def home_user():
-            if "username" in session:
-                self.username = session["username"]
+            # If user session exist, save the session in class and return HTML page
+            if self.smb_server._user in session:
+                self.session = session[self.smb_server._user]
                 return render_template('dir_list.html'), 200
             else:
                 return render_template('bad_login.html'), 401
